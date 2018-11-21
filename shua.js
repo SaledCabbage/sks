0d4860eb4dc89737dbaabb1aa5e73cff565ae080
SaledCabbage <h155811@126.com>
2018-11-21 22:56:42 +0800

    修复错误
    修复错误

--- shua.js
+++ shua.js
@@ -1,4 +1,4 @@
 var sxjs = document.createElement("script");
 sxjs.type = "text/javascript";
-sxjs.src = "https://hwang.site/ke.js"
+sxjs.src = "https://hwang.site/ke.js";
 document.body.appendChild(sxjs);
